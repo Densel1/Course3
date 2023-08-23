217370f15cad8e36a79cfb1dbbb854f6a5a2891d
Igor Kastyrkin <tremen@yandex.ru>
2023-08-23 23:37:04 +0300

    correct come err

--- main/main/ini_parser.h
+++ main/main/ini_parser.h
@@ -2,162 +2,44 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <fstream>
+#include <exception>
 #include <iostream>
 
-using namespace std;
 
-// потом добавлю исключения
+
 class ini_parser
 {
 private:
-	std::multimap<std::string, pair<std::string,string>> sections;
+	std::multimap<std::string, std::pair<std::string, std::string>> sections;
 
 public:
-	ini_parser(std::string name) :fName(name)
+	ini_parser() = delete;
+	ini_parser(const std::multimap<std::string, std::pair<std::string, std::string>>& m)
 	{
-		std::ifstream in(name);
-		std::string line;
-		bool is_reading_section = false;
-		bool is_reading_value = false;
-		std::string section_name;
-		std::string value;
-		std::string value_name;
+		sections = std::move(m);
+		//for (auto it = begin(sections); it != end(sections); ++it)
+		//{
+		//	std::cout << it->first << std::endl;
+		//	std::cout << it->second.first << std::endl;
+		//	std::cout << it->second.second << std::endl;
+		//}
 
-		if (in.is_open())
-		{
-			cout << "file is opened" << endl;
-			while (std::getline(in, line))
-			{
-//				cout << "line " << line << endl;
-				for (auto& s : line)
-				{
-					if (s == ';') break;
-					if (s == '[')
-					{
-						if (!section_name.empty())
-							section_name.erase();
-						is_reading_section = true;
-						continue;
-					}
-					if (s == ']')
-					{
-						is_reading_section = false;
-						continue;
-					}
-					if (s == '=')
-					{
-						is_reading_value = true;
-						continue;
-					}
+	};
 
-					if (is_reading_section)
-						section_name.push_back(s);
-					else if (is_reading_value)
-					{
-						value.push_back(s);
-					}
-					else value_name.push_back(s);
-				}
+	template <typename T>
+	T get_value(const std::string& line);
 
-
-				if (is_reading_value && !value.empty())
-				{
-					std::pair<std::string, std::string> p1;
-                    p1 = make_pair(value_name, value);
-		//			cout << "sec_name: " << section_name << endl;
-
-		//			cout << "val_name: " << value_name << endl;
-
-					sections.insert(pair<string, pair<string, string>>(section_name, p1));
-
-					value.erase();
-				}
-				is_reading_value = false;
-				value_name.erase();
-			}
-		}
-		else { cout << "can't open file" << endl; }
-	}
-
-	std::string fName;
-
-	// тут мне не понятно как возвращать значение которое выставлено в параметре шаблона
-	// ведь заранее не известно какого типа у нас переменная
-	//template <typename T>
-	string get_value(std::string line)
-	{
-	  string sectoin_name;
-	  string str;
-	  
-	  bool point_char = false;
-	  int i;
-	  string sel;
-	  string val;
-	  for (auto& s : line)
-	  {
-	    if(s == '.'){
-		  point_char = true;
-		  continue;
-		}
-	    if(!point_char)
-		{
-			sel.push_back(s);
-		}
-		else { val.push_back(s); }
-	  }
-
-	  multimap<string, pair<string, string>>::iterator it = begin(sections);
-	  for(; it!=end(sections);it++)
-	  {
-		if((it->first)==sel)
-		{
-		  if(val == (it->second.first))
-		  {
-			  {
-				//  cout << "Section: " << it->first << endl;
-				//  cout << it->second.first << ":"  << it->second.second << endl;
-				  str = it->second.second;
-			  }
-		  }
-		  return str;
-		}
-	  }
-	  return " ";
-	}
-	
-
-	template<typename T> void print_value(T value)
-	{
-		cout << "Value: " << static_cast<const char*>(&value) << std::endl;
-	}
-
+	~ini_parser() = default;
 
 private:
 
-	
-
-	std::string read_string(std::string my_str)
-	{
-		std::string val;
-		for (unsigned i = 0; i < my_str.length(); ++i)
-		{
-			if (my_str.at(i) == ';') break;
-			if (my_str.at(i) == '=')
-			{
-			  for(int j = 0; (j!='\n')&&(j!=';');j++)
-			  {
-				  val.at(j) = my_str.at(j + i);
-			  }
-			}
-		}
-      return val;
-	}
 
 	bool is_number(const std::string& s)
 	{
 		std::string::const_iterator it = s.begin();
-		while (it != s.end() && std::isdigit(*it)) ++it;
+		while (it != s.end() && (std::isdigit(*it) || (*it == '.'))) { 	++it;
+		}
+		it++;
 		return !s.empty() && it == s.end();
 	}
 };
--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,10 +1,89 @@
 ﻿
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
 #include "ini_parser.h"
+
+
+void read_file(std::string name, std::multimap<std::string, std::pair<std::string, std::string>> &m)
+{
+	std::ifstream in(name);
+	std::string line;
+	bool is_reading_section = false;
+	bool is_reading_value = false;
+	std::string section_name;
+	std::string value;
+	std::string value_name;
+
+	if (in.is_open())
+	{
+		while (std::getline(in, line))
+		{
+			//				cout << "line " << line << endl;
+			for (auto& s : line)
+			{
+				if (s == ';') break;
+				if (s == '[')
+				{
+					if (!section_name.empty())
+						section_name.erase();
+					is_reading_section = true;
+					continue;
+				}
+				if (s == ']')
+				{
+					is_reading_section = false;
+					continue;
+				}
+				if (s == '=')
+				{
+					is_reading_value = true;
+					continue;
+				}
+
+				if (is_reading_section)
+					section_name.push_back(s);
+				else if (is_reading_value)
+				{
+					value.push_back(s);
+				}
+				else value_name.push_back(s);
+			}
+
+
+			if (is_reading_value && !value.empty())
+			{
+
+			//	std::cout << value << std::endl;
+
+				std::pair<std::string, std::string> p1(value_name, value);
+
+				m.insert(std::pair<std::string, std::pair<std::string, std::string>>(section_name, p1));
+
+				value.erase();
+			}
+			is_reading_value = false;
+			value_name.erase();
+		}
+	}
+	else  throw std::exception("can't open file"); //std::cout << "can't open file" << std::endl; }
+	in.close();
+}  
+
+
 
 int main()
 {
-	ini_parser parser("filename.ini");
-	auto value = parser.get_value("Section1.var1");
-    std::cout << "value = " << value << endl;
+	try{
+    std::multimap<std::string, std::pair<std::string, std::string>> m;
+	read_file("filename.ini", m);
+	ini_parser parser(m);
+	auto value = parser.get_value<int>("Section1.var2");
+    std::cout << "VALUE = " << value << std::endl;
+	}
+	catch (const std::exception& ex) { std::cout << ex.what() << std::endl; };
+//	catch (...) { std::cout << "unknown error"; }
+	return 0;
 }
