dfb4a7d337ab5a6e41df4fc78bdc1684802bc194
Igor Kastyrkin <tremen@yandex.ru>
2023-08-26 00:55:40 +0300

    correct come err

--- main/main/ini_parser.cpp
+++ main/main/ini_parser.cpp
@@ -1,62 +1,10 @@
 #include "ini_parser.h"
 
 
-
-template <typename T>
-T ini_parser::get_value(const std::string &line)
+bool ini_parser::is_number(const std::string& s)
 {
-	//for (auto it = begin(sections); it != end(sections); ++it)
-	//{
-	//	std::cout << it->first << std::endl;
-	//	std::cout << it->second.first << std::endl;
-	//	std::cout << it->second.second << std::endl;
-	//}
+	std::string::const_iterator it = s.begin();
+	while (it != s.end() && (std::isdigit(*it) || (*it == '.')||(*it == ' '))) ++it;
 
-	std::string sectoin_name;
-	std::string str = "";
-
-	bool point_char = false;
-	//section
-	std::string sel;
-	//value
-	std::string val;
-	for (auto& s : line)
-	{
-		if (s == '.') {
-			point_char = true;
-			continue;
-		}
-		if (!point_char)
-		{
-			sel.push_back(s);
-		}
-		else { val.push_back(s); }
-
-	}
-	//		std::cout << sel << std::endl;
-	//		std::cout << val << std::endl;
-	//		std::multimap<std::string, std::pair<std::string, std::string>>::iterator it = begin(sections);
-	for (auto it = begin(sections); it != end(sections); it++)
-	{
-		//			std::cout << "noop" << std::endl;
-		if ((it->first) == sel)
-		{
-			//				std::cout << sel << std::endl;
-			if (val == (it->second.first))
-			{
-				//					std::cout << "read " << val << std::endl;
-				str = it->second.second;
-				//					std::cout << str << std::endl;
-			}
-			//				else std::cout << val << " != " << it->second.first << std::endl;
-		}
-		// this works in C++17
-	}
-	if (str == "") throw std::exception("empty data");
-	if constexpr (std::is_same_v<T, std::string>) return str;
-	else if constexpr (std::is_same_v<T, int>)
-	{
-		if (is_number(str)) return atoi(str.c_str());
-		else throw std::exception("not a number");
-	}
+	return !s.empty() && it == s.end();
 }
--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,10 +1,12 @@
 ﻿
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
 #include <map>
 #include <string>
-#include <vector>
+#include "myException.h"
 #include "ini_parser.h"
+
 
 
 void read_file(std::string name, std::multimap<std::string, std::pair<std::string, std::string>> &m)
@@ -13,15 +15,18 @@ void read_file(std::string name, std::multimap<std::string, std::pair<std::strin
 	std::string line;
 	bool is_reading_section = false;
 	bool is_reading_value = false;
+	bool delete_spases = true;
 	std::string section_name;
 	std::string value;
 	std::string value_name;
+	unsigned string_number = 0;
+	bool first_section = 0;
 
 	if (in.is_open())
 	{
 		while (std::getline(in, line))
 		{
-			//				cout << "line " << line << endl;
+			string_number++;
 			for (auto& s : line)
 			{
 				if (s == ';') break;
@@ -29,42 +34,63 @@ void read_file(std::string name, std::multimap<std::string, std::pair<std::strin
 				{
 					if (!section_name.empty())
 						section_name.erase();
-					is_reading_section = true;
+					if (is_reading_section)
+					{
+						throw myException("duplicate bracket in line:", string_number);
+					}
+					if (!is_reading_value) is_reading_section = true;
 					continue;
 				}
 				if (s == ']')
 				{
-					is_reading_section = false;
-					continue;
+					if (is_reading_section)
+					{
+						is_reading_section = false;
+						first_section = true;
+						continue;
+					}
+					else throw myException("wrong symbol at line:", string_number);
 				}
 				if (s == '=')
 				{
 					is_reading_value = true;
 					continue;
 				}
-
+				if (s == ' ')
+				{
+					if (delete_spases) continue;
+				}
+				if (s != ' ')
+				{
+					if (is_reading_value&&delete_spases) {
+						delete_spases = false;
+					}
+				}
 				if (is_reading_section)
 					section_name.push_back(s);
 				else if (is_reading_value)
 				{
-					value.push_back(s);
+					if(!delete_spases) value.push_back(s);
 				}
-				else value_name.push_back(s);
+				else
+				{
+					if (first_section)
+					{
+						if (s != ' ') value_name.push_back(s);
+					}
+					else throw myException("no section for value in line: ", string_number);
+				}
 			}
 
 
 			if (is_reading_value && !value.empty())
 			{
-
-			//	std::cout << value << std::endl;
-
 				std::pair<std::string, std::string> p1(value_name, value);
-
 				m.insert(std::pair<std::string, std::pair<std::string, std::string>>(section_name, p1));
-
 				value.erase();
 			}
 			is_reading_value = false;
+			delete_spases = true;
 			value_name.erase();
 		}
 	}
@@ -80,10 +106,20 @@ int main()
     std::multimap<std::string, std::pair<std::string, std::string>> m;
 	read_file("filename.ini", m);
 	ini_parser parser(m);
-	auto value = parser.get_value<int>("Section1.var2");
+	auto value = parser.get_value<int>("Section1.var1");
     std::cout << "VALUE = " << value << std::endl;
+	auto value1 = parser.get_value<std::string>("Section1.var2");
+	std::cout << "VALUE1 = " << value1 << std::endl;
+	auto value2 = parser.get_value<int>("Section2.var1");
+	std::cout << "VALUE2 = " << value2 << std::endl;
+	auto value3 = parser.get_value<std::string>("Section2.var2");
+	std::cout << "VALUE3 = " << value3 << std::endl;
+	auto value4 = parser.get_value<std::string>("Section1.var1");
+	std::cout << "VALUE4 = " << value4 << std::endl;
+	auto value5 = parser.get_value<std::string>("Section2.val1");
+	std::cout << "VALUE5 = " << value5 << std::endl;
 	}
-	catch (const std::exception& ex) { std::cout << ex.what() << std::endl; };
-//	catch (...) { std::cout << "unknown error"; }
+	catch (const std::exception& ex) { std::cout << ex.what() << std::endl; }
+	catch (...) { std::cout << "unknown error" << std::endl; }
 	return 0;
 }
